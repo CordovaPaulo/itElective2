cca4ef8fc2dea22f491e1acb59ee854bb7be2d44
CordovaPaulo <202311378@gordoncollege.edu.ph>
2025-08-31 00:26:10 +0800

    better update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.getElementById('flip_Back_Btn_front').addEventListener('click', function() {
+const remove_btn = document.getElementById('remove-cover');
+
+document.getElementById('flip_Front_Btn').addEventListener('click', function() {
     document.querySelector('.flip-card').classList.add('flipped');
 });
+
 document.getElementById('flip_Back_Btn').addEventListener('click', function() {
     document.querySelector('.flip-card').classList.remove('flipped');
+});
+
+remove_btn.addEventListener('click', function() {
+    document.querySelector('.background').classList.add('level');
+    remove_btn.classList.add('hide'); // Hide the button smoothly
 });
